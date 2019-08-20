7e5ca3ded5b6ddd5e3b948e933c7a06723dc1469
MinKyeongTae <kyeongtea92@gmail.com>
2019-08-20 23:55:40 +0900

    Swift Algorithm Challenge Study

---
+++ SwiftAlgorithmStudy.playground/Contents.swift
@@ -0,0 +1,157 @@
+import UIKit
+
+////// MARK:- SWIFT ALGORITHM CHALLENGE STUDY
+
+//// MARK: - Challenge 1
+/// 본 문자열 내의 중복문자가 있는지 확인하라
+
+// var strDic = Set<Character>()
+// var flag = true
+//
+// func challenge0(input: String) -> Bool {
+//    return (input.count == Set(input).count)
+// }
+//
+// func challenge1(_ input: String) -> Bool {
+//
+//    for char in input {
+//        if strDic.contains(char) == false {
+//            strDic.insert(char)
+//        } else {
+//            return false
+//        }
+//    }
+//    return true
+// }
+//
+// print(challenge1("aas"))
+
+//// MARK: - Challenge 2
+///// 본 문자열과 reversed 문자열이 동일한지 확인하라
+//
+// func challenge3(_ input: String) -> Bool {
+//    return String(input.lowercased()) == String(input.lowercased().reversed())
+// }
+//
+// print(challenge3("Rats live on no evil star"))
+
+//// MARK: - Challenge 3
+//
+// var strDic = [Character:Int]()
+// var strDic2 = [Character:Int]()
+//
+//
+// func challenge4(input: String, input2: String) -> Bool {
+//    let str = input.replacingOccurrences(of: " ", with: "").sorted()
+//    let str2 = input2.replacingOccurrences(of: " ", with: "").sorted()
+//    print(str)
+//    print(str2)
+//    return str == str2
+// }
+//
+////✭ component 구분자 + separated + joined() 를 사용할 수도 있다.
+//// remove를 통한 공백 제거 후 sorted()
+// print(challenge4(input: "a1b2", input2: "1a b2"))
+
+// let str = "asd"
+// print(str.fuzzyContains(input: "asd"))
+
+//// 첫번째 방법
+// extension String {
+//    func fuzzyContains(input: String) -> Bool {
+//        return self.lowercased().range(of: input.lowercased()) != nil
+//    }
+// }
+
+//// 두번째 방법
+// extension String {
+//    func fuzzyContains(input: String) -> Bool {
+//
+//                let mStr = Array(self.lowercased()) // 접근한 String
+//                let lowercasedStr = input.lowercased()
+//                var index: String.Index
+//
+//                for char in mStr {
+//                    var index = char == mStr[index] ? mStr.index(after: index) : mStr.startIndex
+//
+//                }
+//
+//        if index == str.endIndex {
+//            return true
+//        }
+//
+//        return false
+//    }
+// }
+
+//// MARK: - Challenge 5
+
+//// 1번째 방법
+// print(verifyString(char: "i", str2: "Hacking With Swift"))
+// func verifyString(char: Character, str2: String) -> Int {
+//    var count = 0
+//    for ch in str2 {
+//        if char == ch {
+//            count += 1
+//        }
+//    }
+//    return count
+// }
+//
+// func verifyString(char: Character, str2: String) -> Int {
+//    var count = 0
+//
+//    return str2.characters.reduce(0) {
+//        $0 == count ? $0 + 1 : $0
+//    }
+// }
+
+////// MARK: - Challenge 6
+
+var str = "Hello, playground"
+// var dict = [Character:Bool]()
+var setData = Set<Character>()
+func checkDuplicate(string: String) -> String {
+    //    string.forEach { (char) in
+    //        dict[char] = false
+    //    }
+    //
+    //    var ansString: String = ""
+    //    string.forEach { (char) in
+    //        guard let chk = dict[char] else { return }
+    //        if !chk {
+    //            dict[char] = true
+    //            ansString.append(char)
+    //        }
+    //    }
+
+    return string.filter { (char) -> Bool in
+        if setData.contains(char) {
+            return false
+        } else {
+            setData.insert(char)
+            return true
+        }
+    }
+}
+
+// ✔︎ contains를 활용한 문제 풀이 가능하다.
+
+print(checkDuplicate(string: "Mississippi"))
+
+func challenge6a(string: String) -> String {
+    let array = string.map { String($0) }
+    let set = NSOrderedSet(array: array)
+    let letters = Array(set) as! [String]
+    return letters.joined() // 빈칸없는 String으로 정리한 뒤 출력 -> 정답!
+}
+
+func challenge6c(string: String) -> String {
+    var used = [Character: Bool]()
+
+    let result = string.filter {
+        used.updateValue(true, forKey: $0) == nil
+    }
+
+    return result
+}
