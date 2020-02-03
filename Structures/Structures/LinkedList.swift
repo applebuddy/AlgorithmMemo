9804fb9aaa4ef7a39f7490ab410ecdfbd92c9bdc
MinKyeongTae <kyeongtea92@gmail.com>
2020-02-03 20:09:17 +0900

    Arraning structure project group Folders

---
+++ Structures/Structures/LinkedList.swift
@@ -0,0 +1,41 @@
+//
+//  LinkedList.swift
+//  Structures
+//
+//  Created by MinKyeongTae on 2020/02/03.
+//  Copyright © 2020 MinKyeongTae. All rights reserved.
+//
+
+// MARK: LinkedList
+
+// MARK: - 기본 연결리스트 구조 구현
+
+/*
+ import Foundation
+
+ class Node<T> {
+ var value: Int
+ var next: Node?
+ init(_ value: Int, _ next: Node? = nil) {
+     self.value = value
+     self.next = next
+ }
+ }
+
+ extension Node: CustomStringConvertible {
+ var description: String {
+     guard let next = self.next else {
+         return "\(value)"
+     }
+
+     return "\(value) -> \(next.description) "
+ }
+ }
+
+ var node = Node<Int>(1)
+ var node2 = Node<Int>(2)
+ var node3 = Node<Int>(3)
+ node.next = node2
+ node2.next = node3
+ print(node.description)
+ */
--- Structures/Structures/mainc.swift
+++ Structures/Structures/mainc.swift
@@ -6,36 +6,36 @@
 ////  Copyright © 2019 MinKyeongTae. All rights reserved.
 ////
 //
-//import Foundation
+// import Foundation
 //
-//print("Hello, World!")
+// print("Hello, World!")
 //
 //// MARK: - Queue VS DoubleStack
 //
 //// MARK: QueueArray
 //
-//var queueArray = QueueArray<Int>()
-//queueArray.push(1)
-//queueArray.push(2)
-//queueArray.push(3)
-//print(queueArray)
-//queueArray.pop()
-//print(queueArray)
-//queueArray.pop()
-//print(queueArray)
-//queueArray.pop()
-//print(queueArray)
+// var queueArray = QueueArray<Int>()
+// queueArray.push(1)
+// queueArray.push(2)
+// queueArray.push(3)
+// print(queueArray)
+// queueArray.pop()
+// print(queueArray)
+// queueArray.pop()
+// print(queueArray)
+// queueArray.pop()
+// print(queueArray)
 //
 //// MARK: DoubleStack
 //
-//var doubleStack = QueueDoubleStack<Int>()
-//doubleStack.push(1)
-//doubleStack.push(2)
-//doubleStack.push(3)
-//print(doubleStack)
-//doubleStack.pop()
-//print(doubleStack)
-//doubleStack.pop()
-//print(doubleStack)
-//doubleStack.pop()
-//print(doubleStack)
+// var doubleStack = QueueDoubleStack<Int>()
+// doubleStack.push(1)
+// doubleStack.push(2)
+// doubleStack.push(3)
+// print(doubleStack)
+// doubleStack.pop()
+// print(doubleStack)
+// doubleStack.pop()
+// print(doubleStack)
+// doubleStack.pop()
+// print(doubleStack)
