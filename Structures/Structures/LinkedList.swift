8ee4855b8ca90f3f3aa425e049adccc0e676bb35
MinKyeongTae <kyeongtea92@gmail.com>
2020-02-04 18:04:27 +0900

    LinkedList Structure Swift 코드구현 복습

--- Structures/Structures/LinkedList.swift
+++ Structures/Structures/LinkedList.swift
@@ -8,6 +8,41 @@
 
 // MARK: LinkedList
 
+// MARK: - 기본 연결리스트 구조 구현 복습
+
+// Basic LinkedList Structure
+/*
+import Foundation
+
+class LLNode<T> {
+    var value: Int
+    var next: LLNode?
+    
+    init(_ value: Int, _ next: LLNode? = nil) {
+        self.value = value
+        self.next = next
+    }
+}
+
+extension LLNode: CustomStringConvertible {
+    var description: String {
+        guard let next = self.next else {
+            return "\(self.value)"
+        }
+        
+        return "\(self.value) -> \(next.description)"
+    }
+}
+
+var node5 = LLNode<Int>(5)
+var node4 = LLNode<Int>(4, node5)
+var node3 = LLNode<Int>(3, node4)
+var node2 = LLNode<Int>(2, node3)
+var node1 = LLNode<Int>(1, node2)
+
+print(node1.description)
+*/
+
 // MARK: - 기본 연결리스트 구조 구현
 
 /*
